13d09e27d75af7b1528b108df89f7635226a2dc9
Hannes Wattenberg <hannes.wattenberg@realcore.de>
2025-04-02 10:10:19 +0200

    [ADD]: Added AccordeonSection and adjusted styling of other UI components

--- src/components/Interface/InterfaceComponents/AccordeonItem.jsx
+++ src/components/Interface/InterfaceComponents/AccordeonItem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const AccordeonItem = ({type, img, path, createModel}) => {
   return (
-    <button onClick={() => createModel(type, path)} className="h-24 w-24 bg-white rounded-sm shadow">
+    <button onClick={() => createModel(type, path)} className="h-22 w-22 bg-white rounded-sm shadow">
       <img src={"/Pictures/" + img} className="h-full w-full object-cover rounded-sm" />
     </button>
   );
--- src/components/Interface/InterfaceComponents/AccordeonSection.jsx
+++ src/components/Interface/InterfaceComponents/AccordeonSection.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
 
-const AccordeonSection = () => {
+const AccordeonSection = ({sectionTitle='Lmao', children}) => {
   return (
-    null
+    <div className=' w-full p-1 bg-black bg-opacity-5 backdrop-blur-sm rounded-sm'>
+      <div className='text-xs text-slate-900 border-b border-slate-700 leading-none mb-[-2px]'>
+        {sectionTitle}
+      </div>
+      <div className='grid grid-cols-4 place-items-center gap-1 gap-y-1 pt-1.5'>
+        {children}
+      </div>
+    </div>
+    
+    
   );
 };
 
