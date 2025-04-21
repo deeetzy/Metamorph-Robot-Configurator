a63f955712558701923cb5825ec4ef322e0f6eae
Hannes Wattenberg <hannes.wattenberg@realcore.de>
2025-04-22 01:43:16 +0200

    [ADD]: Added new assets

--- src/components/Scene/SceneComponents/Controls.jsx
+++ src/components/Scene/SceneComponents/Controls.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { OrbitControls, CameraControls} from '@react-three/drei';
+import { useRef } from 'react';
 
 const Controls = () => {
+  const cameraRef = useRef();
+
+  useEffect(() => {
+    if (cameraRef.current) {
+      cameraRef.current.setTarget(0,2,0, false);
+    }
+
+  }, []);
+
+
   return (
     <>
         <CameraControls makeDefault
-            minDistance={5}
+          ref={cameraRef}
+            minDistance={2}
             maxDistance={15}
             truckSpeed={0}
         />
