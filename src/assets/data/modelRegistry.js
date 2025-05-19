c1d0bff7deda7a0b03e8ec03d05c87e6a067f8ca
Hannes Wattenberg <wattenbe@uni-bremen.de>
2025-05-19 06:39:28 +0200

    [ADD]: pushed remaining models

--- src/assets/data/modelRegistry.js
+++ src/assets/data/modelRegistry.js
@@ -2,7 +2,7 @@
 //TODO: find solution, so shapes are at the beginning of every accordeon without registry code duplication
 const modelRegistry = {
     'Core Subdivision':{
-        'General descriptor - Shapes':{
+        'Basic Shapes':{
             Barrel: {
                 name: 'Barrel',
                 type: 'core subdivision',
@@ -17,23 +17,29 @@ const modelRegistry = {
                 path: '/Assets/Cube.glb',
             },
             //inconsistent naming
-            RoundedBox: {
-                name: 'BoxRounded',
+            BoxRounded: {
+                name: 'Rounded Box',
                 type: 'core subdivision',
                 img: 'BoxRounded.png',
-                path: '/Assets/RoundedBox.glb',
+                path: '/Assets/BoxRounded.glb',
+            },
+            ConeGeneral: {
+                name: 'Cone',
+                type: 'core subdivision',
+                img: 'ConeGeneral.png',
+                path: '/Assets/ConeGeneral.glb',
+            },
+            ConeRounded: {
+                name: 'Rounded Cone',
+                type: 'core subdivision',
+                img: 'ConeRounded.png',
+                path: '/Assets/ConeRounded.glb',
             },
             Cog: {
                 name: 'Cog',
                 type: 'core subdivision',
                 img: 'Cog.png',
                 path: '/Assets/Cog.glb',
-            },
-            Cone: {
-                name: 'Cone',
-                type: 'core subdivision',
-                img: 'Cone.png',
-                path: '/Assets/Cone.glb',
             },
             Cylinder: {
                 name: 'Cylinder',
@@ -126,14 +132,70 @@ const modelRegistry = {
                 path: '/Assets/VShape.glb',
             },
         },
+        'Letter Shapes':{
+            LShaped: {
+                name: 'L-Shaped',
+                type: 'core subdivision',
+                img: 'LShaped.png',
+                path: '/Assets/LShaped.glb',
+            },
+            SShaped: {
+                name: 'S-Shaped',
+                type: 'core subdivision',
+                img: 'SShaped.png',
+                path: '/Assets/SShaped.glb',
+            },
+            TShaped: {
+                name: 'T-Shaped',
+                type: 'core subdivision',
+                img: 'TShaped.png',
+                path: '/Assets/TShaped.glb',
+            },
+            UShaped: {
+                name: 'U-Shaped',
+                type: 'core subdivision',
+                img: 'UShaped.png',
+                path: '/Assets/UShaped.glb',
+            },
+            XShaped: {
+                name: 'X-Shaped',
+                type: 'core subdivision',
+                img: 'XShaped.png',
+                path: '/Assets/XShaped.glb',
+            },
+        },
     },
     'Connecting Subdivision':{
         'Basic connecting subdivisions':{
-            Neck: {
-                name: 'Neck',
+            Head: {
+                name: 'Head',
                 type: 'connecting subdivision',
-                img: 'Neck.png',
-                path: '/Assets/ShortNeck.glb',
+                img: 'Head.png',
+                path: '/Assets/Head.glb',
+            },
+            Helmet: {
+                name: 'Helmet',
+                type: 'connecting subdivision',
+                img: 'Helmet.png',
+                path: '/Assets/Helmet.glb',
+            },
+            Snout: {
+                name: 'Snout',
+                type: 'connecting subdivision',
+                img: 'Snout.png',
+                path: '/Assets/Snout.glb',
+            },
+            NeckShort: {
+                name: 'Short Neck',
+                type: 'connecting subdivision',
+                img: 'NeckShort.png',
+                path: '/Assets/NeckShort.glb',
+            },
+            NeckLong: {
+                name: 'Long Neck',
+                type: 'connecting subdivision',
+                img: 'NeckLong.png',
+                path: '/Assets/NeckLong.glb',
             },
             Shoulder: {
                 name: 'Shoulder',
@@ -161,6 +223,12 @@ const modelRegistry = {
                 img: 'Arm2.png',
                 path: '/Assets/Arm2.glb',
             },
+            Arm3: {
+                name: '3-Joint Arm',
+                type: 'connecting subdivision',
+                img: 'Arm3.png',
+                path: '/Assets/Arm3.glb',
+            },
         },
         'Leg configurations':{
             Leg0: {
@@ -181,44 +249,56 @@ const modelRegistry = {
                 img: 'Leg2.png',
                 path: '/Assets/Leg2.glb',
             },
+            Leg3: {
+                name: '3-Joint Leg',
+                type: 'connecting subdivision',
+                img: 'Leg3.png',
+                path: '/Assets/Leg3.glb',
+            },
         },
     },
     'Terminal Subdivision':{
-        'Manipulator':{
-            Hand2: {
-                name: '2-finger Hand',
+        'Hair':{
+            HairA: {
+                name: 'Hair Long',
                 type: 'terminal subdivision',
-                img: 'Hand2.png',
-                path: '/Assets/Hand2.glb',
+                img: 'HairA.png',
+                path: '/Assets/HairA.glb',
             },
-            Hand3: {
-                name: '3-finger Hand',
+            HairB: {
+                name: 'Hair Short',
                 type: 'terminal subdivision',
-                img: 'Hand3.png',
-                path: '/Assets/Hand3.glb',
+                img: 'HairB.png',
+                path: '/Assets/HairB.glb',
             },
-            Hand4: {
-                name: '4-finger Hand',
+            Eyebrow: {
+                name: 'Eyebrow',
                 type: 'terminal subdivision',
-                img: 'Hand4.png',
-                path: '/Assets/Hand4.glb',
+                img: 'Eyebrow.png',
+                path: '/Assets/Eyebrow.glb',
             },
-            Hand5: {
-                name: '5-finger Hand',
+            Beard: {
+                name: 'Beard',
                 type: 'terminal subdivision',
-                img: 'Hand5.png',
-                path: '/Assets/Hand5.glb',
+                img: 'Beard.png',
+                path: '/Assets/Beard.glb',
             },
         },
-        'Higher face feature':{
+        'Higher Face Features':{
+            Eye: {
+                name: 'Eye',
+                type: 'terminal subdivision',
+                img: 'Eye.png',
+                path: '/Assets/Eye.glb',
+            },
             CameraEye: {
-                name: 'Camera eye',
+                name: 'Camera Eye',
                 type: 'terminal subdivision',
                 img: 'CameraEye.png',
                 path: '/Assets/CameraEye.glb',
             },
             SensorArray: {
-                name: 'Sensor array',
+                name: 'Sensor Array',
                 type: 'terminal subdivision',
                 img: 'SensorArray.png',
                 path: '/Assets/SensorArray.glb',
@@ -229,33 +309,119 @@ const modelRegistry = {
                 img: 'Visor.png',
                 path: '/Assets/Visor.glb',
             },
-            Eyebrow: {
-                name: 'Eyebrow',
+        },
+        'Mid Face Features':{
+            Ear1: {
+                name: 'Humanoid Ear',
                 type: 'terminal subdivision',
-                img: 'Eyebrow.png',
-                path: '/Assets/Eyebrow.glb',
+                img: 'Ear1.png',
+                path: '/Assets/Ear1.glb',
+            },
+            Nose1: {
+                name: 'Humanoid Nose',
+                type: 'terminal subdivision',
+                img: 'Nose1.png',
+                path: '/Assets/Nose1.glb',
+            },
+            Ear2: {
+                name: 'Robotic Ear',
+                type: 'terminal subdivision',
+                img: 'Ear2.png',
+                path: '/Assets/Ear2.glb',
+            },
+            Nose2: {
+                name: 'Robotic Nose',
+                type: 'terminal subdivision',
+                img: 'Nose2.png',
+                path: '/Assets/Nose2.glb',
             },
         },
-        'Ground supporting subdivision': {
-            Shoe: {
-                name: '2-Shoe',
+        'Lower Face Features':{
+            Mouth: {
+                name: 'Mouth',
                 type: 'terminal subdivision',
-                img: 'Shoe.png',
-                path: '/Assets/Shoe.glb',
+                img: 'Mouth.png',
+                path: '/Assets/Mouth.glb',
             },
-            TwoToed: {
-                name: '2-Toed',
+            Beak: {
+                name: 'Beak',
                 type: 'terminal subdivision',
-                img: '2Toed.png',
-                path: '/Assets/2Toed.glb',
+                img: 'Beak.png',
+                path: '/Assets/Beak.glb',
             },
-            Hoof: {
-                name: '2-Hoof',
+        },
+        'Hand Configurations':{
+            Hand2: {
+                name: '2-Finger Hand',
                 type: 'terminal subdivision',
-                img: 'Hoof.png',
-                path: '/Assets/Hoof.glb',
+                img: 'Hand2.png',
+                path: '/Assets/Hand2.glb',
+            },
+            Hand3: {
+                name: '3-Finger Hand',
+                type: 'terminal subdivision',
+                img: 'Hand3.png',
+                path: '/Assets/Hand3.glb',
+            },
+            Hand4: {
+                name: '4-Finger Hand',
+                type: 'terminal subdivision',
+                img: 'Hand4.png',
+                path: '/Assets/Hand4.glb',
+            },
+            Hand5: {
+                name: '5-Finger Hand',
+                type: 'terminal subdivision',
+                img: 'Hand5.png',
+                path: '/Assets/Hand5.glb',
+            },
+            Mitten: {
+                name: 'Mitten Hand',
+                type: 'terminal subdivision',
+                img: 'Mitten.png',
+                path: '/Assets/Mitten.glb',
             },
 
+        },
+        'Gripper Configurations':{
+            Gripper2: {
+                name: '2-Finger Gripper',
+                type: 'terminal subdivision',
+                img: 'Gripper2.png',
+                path: '/Assets/Gripper2.glb',
+            },
+            Gripper3: {
+                name: '3-Finger Gripper',
+                type: 'terminal subdivision',
+                img: 'Gripper3.png',
+                path: '/Assets/Gripper3.glb',
+            },
+            Gripper4: {
+                name: '4-Finger Gripper',
+                type: 'terminal subdivision',
+                img: 'Gripper4.png',
+                path: '/Assets/Gripper4.glb',
+            },
+            Gripper5: {
+                name: '5-Finger Gripper',
+                type: 'terminal subdivision',
+                img: 'Gripper5.png',
+                path: '/Assets/Gripper5.glb',
+            },
+        },
+        'Other Manipulators':{
+            Knob: {
+                name: 'Knob',
+                type: 'terminal subdivision',
+                img: 'Knob.png',
+                path: '/Assets/Knob.glb',
+            },
+            SuctionCup: {
+                name: 'Suction Cups',
+                type: 'terminal subdivision',
+                img: 'SuctionCup.png',
+                path: '/Assets/SuctionCup.glb',
+            },
         },
         'Appendage':{
             Antenna: {
@@ -270,6 +436,12 @@ const modelRegistry = {
                 img: 'Backpack.png',
                 path: '/Assets/Backpack.glb',
             },
+            CableBundle: {
+                name: 'CableBundle',
+                type: 'terminal subdivision',
+                img: 'CableBundle.png',
+                path: '/Assets/CableBundle.glb',
+            },
             Container: {
                 name: 'Container',
                 type: 'terminal subdivision',
@@ -282,14 +454,32 @@ const modelRegistry = {
                 img: 'Fin.png',
                 path: '/Assets/Fin.glb',
             },
+            Frame: {
+                name: 'Frame',
+                type: 'terminal subdivision',
+                img: 'Frame.png',
+                path: '/Assets/Frame.glb',
+            },
+            Handle: {
+                name: 'Handle',
+                type: 'terminal subdivision',
+                img: 'Handle.png',
+                path: '/Assets/Handle.glb',
+            },
             FaceScreen: {
-                name: 'FaceScreen',
+                name: 'Screen',
                 type: 'terminal subdivision',
                 img: 'FaceScreen.png',
                 path: '/Assets/FaceScreen.glb',
             },
+            Lamp: {
+                name: 'Lamp',
+                type: 'terminal subdivision',
+                img: 'Lamp.png',
+                path: '/Assets/Lamp.glb',
+            },
             SteeringWheel: {
-                name: 'SteeringWheel',
+                name: 'Steering Wheel',
                 type: 'terminal subdivision',
                 img: 'SteeringWheel.png',
                 path: '/Assets/SteeringWheel.glb',
@@ -299,6 +489,59 @@ const modelRegistry = {
                 type: 'terminal subdivision',
                 img: 'Tail.png',
                 path: '/Assets/Tail.glb',
+            },
+
+        },
+        'Ground Supporting Subdivision':{
+            Shoe: {
+                name: 'Shoe',
+                type: 'terminal subdivision',
+                img: 'Shoe.png',
+                path: '/Assets/Shoe.glb',
+            },
+            TwoToed: {
+                name: '2-Toed Foot',
+                type: 'terminal subdivision',
+                img: '2Toed.png',
+                path: '/Assets/2Toed.glb',
+            },
+            ThreeClawed: {
+                name: '3-Clawed Paw',
+                type: 'terminal subdivision',
+                img: '3Clawed.png',
+                path: '/Assets/3Clawed.glb',
+            },
+            Hoof: {
+                name: 'Robotic Hoof',
+                type: 'terminal subdivision',
+                img: 'Hoof.png',
+                path: '/Assets/Hoof.glb',
+            },
+            Wheel: {
+                name: 'Wheel',
+                type: 'terminal subdivision',
+                img: 'Wheel.png',
+                path: '/Assets/Wheel.glb',
+            },
+            Tread: {
+                name: 'Tread',
+                type: 'terminal subdivision',
+                img: 'Tread.png',
+                path: '/Assets/Tread.glb',
+            },
+            Stand: {
+                name: 'Stand',
+                type: 'terminal subdivision',
+                img: 'Stand.png',
+                path: '/Assets/Stand.glb',
+            },
+        },
+        'Air Supporting Subdivision':{
+            AirRotor: {
+                name: 'Air Rotor',
+                type: 'terminal subdivision',
+                img: 'AirRotor.png',
+                path: '/Assets/AirRotor.glb',
             },
             Thruster: {
                 name: 'Thruster',
@@ -311,6 +554,40 @@ const modelRegistry = {
                 type: 'terminal subdivision',
                 img: 'Wing.png',
                 path: '/Assets/Wing.glb',
+            },
+        },
+        'Water Supporting Subdivision':{
+            WaterRotor: {
+                name: 'Water Rotor',
+                type: 'terminal subdivision',
+                img: 'WaterRotor.png',
+                path: '/Assets/WaterRotor.glb',
+            },
+            Rudder: {
+                name: 'Rudder',
+                type: 'terminal subdivision',
+                img: 'Rudder.png',
+                path: '/Assets/Rudder.glb',
+            },
+        },
+        'Rope Supporting Subdivision':{
+            Hanger: {
+                name: 'Hanger',
+                type: 'terminal subdivision',
+                img: 'Hanger.png',
+                path: '/Assets/Hanger.glb',
+            },
+            PulleyWheel: {
+                name: 'Pulley Wheel',
+                type: 'terminal subdivision',
+                img: 'PulleyWheel.png',
+                path: '/Assets/PulleyWheel.glb',
+            },
+            Rope: {
+                name: 'Rope',
+                type: 'terminal subdivision',
+                img: 'Rope.png',
+                path: '/Assets/Rope.glb',
             },
         },
     },
